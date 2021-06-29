1158a17f6c2022b73dea724386b4a2f59c246cdb
Rui Fernando Hayashi <rfhayashi@gmail.com>
2021-06-29 19:05:54 -0300

    add repo entity

--- test/clj_github_mock/generators.clj
+++ test/clj_github_mock/generators.clj
@@ -17,16 +17,34 @@
 (defn schema []
   {:org {:prefix :org
          :malli-schema [:map
-                        [:org/name [:string {:gen/gen (unique-name-gen)}]]]}})
+                        [:org/name [:string {:gen/gen (unique-name-gen)}]]]}
+   :repo {:prefix :repo
+          :malli-schema [:map
+                         [:repo/name [:string {:gen/gen (unique-name-gen)}]]]
+          :relations {:repo/org [:org :org/name]}}})
 
 (defn malli-gen-ent-val
   [ent-db {:keys [ent-name]}]
   (let [{:keys [malli-schema]} (sm/ent-schema ent-db ent-name)]
     (mg/generate malli-schema)))
 
+(defn foreign-key-ent [[_ foreign-key-attr :as path] foreign-key-val]
+  (cond
+                                        ; TODO: use constraints to detect if it is a multi relationship
+    (vector? foreign-key-val) (into #{} (map (partial foreign-key-ent path) foreign-key-val))
+    :else {foreign-key-attr foreign-key-val}))
+
+(defn assoc-ent-at-foreign-keys [db {:keys [ent-type visit-key visit-val] :as visit-data}]
+  (reduce
+   (fn [acc [attr relation-path]]
+     (update acc attr (partial foreign-key-ent relation-path)))
+   visit-val
+   (-> db :schema ent-type :relations)))
+
 (def malli-gen [malli-gen-ent-val
                 sg/spec-gen-merge-overwrites
-                sg/spec-gen-assoc-relations])
+                sg/spec-gen-assoc-relations
+                assoc-ent-at-foreign-keys])
 
 (defn ent-db-malli-gen
   [ent-db query]
@@ -38,6 +56,6 @@
 
 (defn database-gen [query]
   (let [database (database/create {})]
-    (-> (ent-db-malli-gen {:schema (schema)} query)
-        (sm/visit-ents-once :inserted-data (partial insert database)))
-    database))
+    {:conn database
+     :ent-db (-> (ent-db-malli-gen {:schema (schema)} query)
+                 (sm/visit-ents-once :inserted-data (partial insert database)))}))
