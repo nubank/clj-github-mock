fe17d2700766f121152b9dc753352a84e9b61d63
Rui Fernando Hayashi <rfhayashi@gmail.com>
2021-08-20 16:53:09 -0300

    add interceptors

--- src/clj_github_mock/http.clj
+++ src/clj_github_mock/http.clj
@@ -1,10 +1,20 @@
 (ns clj-github-mock.http
   (:require [datascript.core :as d]))
 
+(defn api-interceptor [api]
+  {:name ::api-interceptor
+   :api (into {} (d/touch api))
+   :enter (fn [context]
+            (update context :request assoc :api api))})
+
 (defn- api-routes [api]
   (apply conj
-         [(:api/path api) (get api :api/config {})]
-         (map api-routes (sort-by :db/id (:api/_parent api)))))
+         [(:api/path api) (-> api
+                              (get :api/config {})
+                              (update :interceptors #(into [(api-interceptor api)] %)))]
+         (concat
+          (:api/routes api)
+          (map api-routes (sort-by :db/id (:api/_parent api))))))
 
 (defn routes [db]
   (map #(api-routes (d/entity db %)) 
